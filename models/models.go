f5c89b5ec12353f207a04f083a67549a0f90f879
umangraval <umangraval2000@gmail.com>
2020-11-01 23:55:11 +0530

    fix structure

---
+++ models/models.go
@@ -0,0 +1,10 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// Person Model
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
