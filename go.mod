1023a1a6c767d09103a4a83d5a2f7ba620272646
umangraval <umangraval2000@gmail.com>
2020-11-02 00:44:39 +0530

    fix exports and lints

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/umangraval/Go-Mongodb-REST-boilerplate
 go 1.15
 
 require (
+	github.com/aws/aws-sdk-go v1.35.19 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/klauspost/compress v1.11.2 // indirect
 	github.com/rs/cors v1.7.0
 	go.mongodb.org/mongo-driver v1.4.2
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
+	golang.org/x/text v0.3.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/umangraval/Go-Mongodb-REST-boilerplate/controllers"
 )
-
-var client *mongo.Client
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
@@ -19,20 +15,14 @@ func hello(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	fmt.Println("Server running on localhost:8080")
-	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
-	client, _ = mongo.Connect(context.TODO(), clientOptions)
-
 	router := mux.NewRouter()
-	// headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
-	// originsOk := handlers.AllowedOrigins([]string{"https://prog.in"})
-	// methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
-	router.HandleFunc("/person", CreatePersonEndpoint).Methods("POST")
-	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/person/{id}", GetPersonEndpoint).Methods("GET")
-	router.HandleFunc("/person/{id}", DeletePersonEndpoint).Methods("DELETE")
-	router.HandleFunc("/person/{id}", UpdatePersonEndpoint).Methods("PUT")
-	router.HandleFunc("/upload", UploadFileEndpoint).Methods("POST")
+	router.HandleFunc("/person", controllers.CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people", controllers.GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/person/{id}", controllers.GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/person/{id}", controllers.DeletePersonEndpoint).Methods("DELETE")
+	router.HandleFunc("/person/{id}", controllers.UpdatePersonEndpoint).Methods("PUT")
+	router.HandleFunc("/upload", controllers.UploadFileEndpoint).Methods("POST")
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
