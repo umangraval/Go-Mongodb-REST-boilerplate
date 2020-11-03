6029d25a40b25f988075a7567e3584c2e0413e30
umangraval <umangraval2000@gmail.com>
2020-11-03 18:26:54 +0530

    :recycle: response handlers

---
+++ handlers/response.go
@@ -0,0 +1,89 @@
+package middlewares
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/umangraval/Go-Mongodb-REST-boilerplate/models"
+)
+
+// SuccessArrRespond -> response formatter
+func SuccessArrRespond(fields []*models.Person, writer http.ResponseWriter) {
+	// var fields["status"] := "success"
+	_, err := json.Marshal(fields)
+	type data struct {
+		People     []*models.Person `json:"data"`
+		Statuscode int              `json:"status"`
+		Message    string           `json:"msg"`
+	}
+	temp := &data{People: fields, Statuscode: 200, Message: "success"}
+	if err != nil {
+		ServerErrResponse(err.Error(), writer)
+	}
+
+	//Send header, status code and output to writer
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(temp)
+}
+
+// SuccessRespond -> response formatter
+func SuccessRespond(fields models.Person, writer http.ResponseWriter) {
+	_, err := json.Marshal(fields)
+	type data struct {
+		Person     models.Person `json:"data"`
+		Statuscode int           `json:"status"`
+		Message    string        `json:"msg"`
+	}
+	temp := &data{Person: fields, Statuscode: 200, Message: "success"}
+	if err != nil {
+		ServerErrResponse(err.Error(), writer)
+	}
+
+	//Send header, status code and output to writer
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(temp)
+}
+
+// SuccessResponse -> success formatter
+func SuccessResponse(msg string, writer http.ResponseWriter) {
+	type errdata struct {
+		Statuscode int    `json:"status"`
+		Message    string `json:"msg"`
+	}
+	temp := &errdata{Statuscode: 200, Message: msg}
+
+	//Send header, status code and output to writer
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(temp)
+}
+
+// ErrorResponse -> error formatter
+func ErrorResponse(error string, writer http.ResponseWriter) {
+	type errdata struct {
+		Statuscode int    `json:"status"`
+		Message    string `json:"msg"`
+	}
+	temp := &errdata{Statuscode: 400, Message: error}
+
+	//Send header, status code and output to writer
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(writer).Encode(temp)
+}
+
+// ServerErrResponse -> server error formatter
+func ServerErrResponse(error string, writer http.ResponseWriter) {
+	type servererrdata struct {
+		Statuscode int    `json:"status"`
+		Message    string `json:"msg"`
+	}
+	temp := &servererrdata{Statuscode: 500, Message: error}
+
+	//Send header, status code and output to writer
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusInternalServerError)
+	json.NewEncoder(writer).Encode(temp)
+}
